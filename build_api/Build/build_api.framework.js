2a65be145816dbd8bfe3615d95cba9339bf20d70
Deshmukh <shreya.deshmukh@siemens.com>
2023-09-13 16:44:47 +0530

    desert

--- build_api/Build/build_api.framework.js
+++ build_api/Build/build_api.framework.js
@@ -2427,7 +2427,7 @@ function _emscripten_asm_const_id(code, a0) {
  return ASM_CONSTS[code](a0);
 }
 STATIC_BASE = GLOBAL_BASE;
-STATICTOP = STATIC_BASE + 3709568;
+STATICTOP = STATIC_BASE + 3710784;
 __ATINIT__.push({
  func: (function() {
   __GLOBAL__sub_I_AccessibilityScriptingClasses_cpp();
@@ -4501,7 +4501,7 @@ __ATINIT__.push({
   ___emscripten_environ_constructor();
  })
 });
-var STATIC_BUMP = 3709568;
+var STATIC_BUMP = 3710784;
 Module["STATIC_BASE"] = STATIC_BASE;
 Module["STATIC_BUMP"] = STATIC_BUMP;
 var tempDoublePtr = STATICTOP;
